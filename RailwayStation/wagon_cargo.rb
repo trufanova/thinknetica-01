8ab661aab990a0f7512223cebae34029adccb064
Maria Trufanova <5014685@mail.ru>
2023-09-04 21:13:19 +0300

    delete wrong created files

--- RailwayStation/wagon_cargo.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# WagonCargo
-class WagonCargo < Wagon
-  def initialize(number)
-    @type = :cargo
-    super
-  end
-end
--- RailwayStation/wagon_passenger.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# Wagon Passenger
-class WagonPassenger < Wagon
-  def initialize(number)
-    @type = :passenger
-    super
-  end
-end
