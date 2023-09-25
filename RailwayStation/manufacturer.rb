c78ad5746c4f8da63ecdab7b6267205a7ae07bfb
Maria Trufanova <5014685@mail.ru>
2023-09-25 19:27:39 +0300

    refactoring. Change methods for attr_accessor

--- RailwayStation/manufacturer.rb
+++ RailwayStation/manufacturer.rb
@@ -1,10 +1,3 @@
 module Manufacturer
-  def set_manufacturer(value)
-    self.manufacturer = value
-  end
-
-  def show_manufacturer
-    self.manufacturer
-  end
-
+  attr_accessor :manufacturer
 end
